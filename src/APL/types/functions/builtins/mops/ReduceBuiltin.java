548170b1d47424a70828c15bbdef978124ede60b
Marshall Lochbaum <mwlochbaum@gmail.com>
2020-07-02 09:58:17 -0400

    Implement BQN vector reduce (remove array reduction; add dyadic fold)

--- src/APL/types/functions/builtins/mops/ReduceBuiltin.java
+++ src/APL/types/functions/builtins/mops/ReduceBuiltin.java
@@ -17,16 +17,12 @@ public class ReduceBuiltin extends Mop implements DimMMop {
   
   @Override
   public Value call(Obj f, Value w, int dim) {
-    isFn(f);
-    return ngnReduce(w, dim, (Fun)f);
+    throw new DomainError("reduce with axis doesn't exist", this);
   }
   
   public Value call(Value f, Value w, DerivedMop derv) {
-    isFn(f);
-    Fun ff = (Fun) f;
-    if (w.rank >= 2) {
-      return ngnReduce(w, w.rank-1, ff);
-    }
+    Fun ff = f.asFun();
+    if (w.rank != 1) throw new DomainError("argument must have rank 1 (shape ≡ "+Main.formatAPL(w.shape)+")", this, f);
     if (w.quickDoubleArr()) {
       if (f instanceof PlusBuiltin) return new Num(w.sum());
       if (f instanceof MulBuiltin) {
@@ -52,63 +48,21 @@ public class ReduceBuiltin extends Mop implements DimMMop {
     Value[] a = w.values();
     if (a.length == 0) {
       Value id = ff.identity();
-      if (id == null) throw new DomainError("No identity defined for "+f.name(), this, f);
+      if (id == null) throw new DomainError("no identity defined for "+f.name(), this, f);
       return id;
     }
-    Value last = a[a.length-1];
-    for (int i = a.length-2; i >= 0; i--) {
-      last = ff.call(a[i], last);
-    }
-    return last.squeeze();
+    return foldr(ff, a, a[a.length-1], 1);
   }
   
   public Value call(Value f, Value a, Value w, DerivedMop derv) {
-    Fun ff = isFn(f);
-    if (w.rank != 1) throw new NYIError("A f/ B with 2≤⍴⍴B hasn't been implemented", this, w);
-    int n = a.asInt();
-    Value[] ra = new Value[w.ia - Math.abs(n) + 1];
-    Value[] wa = w.values();
-    if (n > 0) {
-      for (int i = 0; i < ra.length; i++) {
-        Value r = wa[i+n-1];
-        for (int j = n-2; j >= 0; j--) {
-          r = ff.call(wa[i + j], r);
-        }
-        ra[i] = r;
-      }
-    } else {
-      n = -n;
-      for (int i = 0; i < ra.length; i++) {
-        Value r = wa[i];
-        for (int j = 1; j < n; j++) {
-          r = ff.call(wa[i + j], r);
-        }
-        ra[i] = r;
-      }
-    }
-    return Arr.create(ra);
+    if (w.rank != 1) throw new DomainError("argument must have rank 1 (shape ≡ "+Main.formatAPL(w.shape)+")", this, f);
+    return foldr(f.asFun(), w.values(), a, 0);
   }
-  
-  private Value ngnReduce(Value x, int axis, Fun f) { // https://chat.stackexchange.com/transcript/message/47158587#47158587
-    if (x.rank == 0) return x;
-    int n0 = 1; // product of all dimensions before "axis"
-    for (int i = 0; i < axis; i++) {
-      n0*= x.shape[i];
+
+  static Value foldr(Fun ff, Value[] a, Value init, int skip) {
+    for (int i = a.length-skip-1; i >= 0; i--) {
+      init = ff.call(a[i], init);
     }
-    int n1 = x.shape[axis]; // the dimension at "axis" - what's getting removed/reduced
-    int n2 = x.ia / (n1*n0); // product of the rest of the shape
-    int[] ns = x.eraseDim(axis);
-    
-    Value[] r = new Value[n0 * n2];
-    for (int i = 0; i < n0; i++) {
-      for (int k = 0; k < n2; k++) {
-        Value c = x.get(i*n1*n2 + (n1-1)*n2 + k);
-        for (int j = n1 - 2; j >= 0; j--) {
-          c = f.call(x.get(i*n1*n2 + j*n2 + k), c);
-        }
-        r[i*n2 + k] = c.squeeze();
-      }
-    }
-    return Arr.create(r, ns);
+    return init.squeeze();
   }
 }
