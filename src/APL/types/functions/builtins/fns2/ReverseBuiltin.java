4fafcc34998315799ec1a765cd4b00691ca7adfc
dzaima <atdzim@inbox.lv>
2020-09-05 02:23:45 +0300

    multidimensional dyadic ⌽

--- src/APL/types/functions/builtins/fns2/ReverseBuiltin.java
+++ src/APL/types/functions/builtins/fns2/ReverseBuiltin.java
@@ -1,9 +1,12 @@
 package APL.types.functions.builtins.fns2;
 
+import APL.Main;
 import APL.errors.DomainError;
 import APL.tools.MutVal;
 import APL.types.*;
 import APL.types.functions.Builtin;
+
+import java.util.Arrays;
 
 public class ReverseBuiltin extends Builtin {
   @Override public String repr() {
@@ -25,7 +28,36 @@ public class ReverseBuiltin extends Builtin {
   
   public Value call(Value w, Value x) {
     if (w instanceof Primitive) return on(w.asInt(), x);
-    throw new DomainError("⌽: 𝕨 must be a scalar number", this, x);
+    int[] wi = w.asIntVec();
+    if (wi.length > x.rank) throw new DomainError("⌽: length of 𝕨 was greater than rank of 𝕩 ("+(Main.formatAPL(x.shape))+" ≡ ≢𝕩, "+Main.formatAPL(wi)+" ≡ 𝕨)", this);
+    wi = Arrays.copyOf(wi, x.rank); // pads with 0s; also creates a mutable copy for moduloing
+    if (x.scalar()) return x; // so recursion doesn't have to worry about it
+  
+    for (int i = 0; i < wi.length; i++) {
+      int l = x.shape[i];
+      if (l==0) return x;
+      int c = wi[i];
+      c%= l; if (c<0) c+= l;
+      wi[i] = c;
+    }
+    
+    MutVal res = new MutVal(x.shape, x);
+    rec(wi, res, x, 0, 0, 0);
+    return res.get();
+  }
+  
+  private void rec(int[] w, MutVal res, Value x, int d, int is, int rs) {
+    int ax = x.shape[d];
+    int mv = w[d];
+    is*= ax;
+    rs*= ax;
+    if (d == x.rank-1) {
+      res.copy(x, is   , rs+ax-mv,    mv);
+      res.copy(x, is+mv, rs      , ax-mv);
+    } else {
+      for (int i =  0; i < mv; i++) rec(w, res, x, d+1, is+i, rs+i+ax-mv);
+      for (int i = mv; i < ax; i++) rec(w, res, x, d+1, is+i, rs+i   -mv);
+    }
   }
   
   @Override public Value callInvW(Value w, Value x) {
